103cdcc09b6db923fc2aca7f1e8b3afba5a7074c
BlowmindFeels <104523257+BlowmindFeels@users.noreply.github.com>
2026-04-05 19:52:53 -0500

    Delete CRUD_maquinas_expendedoras-main directory

--- CRUD_maquinas_expendedoras-main/app/routes/maquina.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-  const maquinas = require("../controllers/maquina.controller");
-
-  app.get("/maquinas", maquinas.findAll);
-  app.post("/maquinas", maquinas.create);
-  app.get("/maquinas/:id", maquinas.findOne);
-  app.put("/maquinas/:id", maquinas.update);
-  app.delete("/maquinas/:id", maquinas.delete);
-};
